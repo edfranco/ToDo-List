2113ed3a2a1373ebf5fb9ffe77c4b17427ea3772
Eduardo Franco <eduardofranco@Eduardos-MacBook-Air.local>
2018-12-28 19:43:30 -0800

    where Intro.js and list.js are put into an if/else to determine which component will render

--- src/App.js
+++ src/App.js
@@ -4,38 +4,31 @@ import Intro from "./components/intro";
 import List from "./components/list";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      name: "",
-      isSubmitted: false
-    }
-  }
-
   
-
-  handleSubmit = (event) => {
+handleSubmit = (event) => {
     this.setState({
       [event.target.name]: event.target.value
     })
 
     this.state.isSubmitted = true;
   }
-/*
+
   handleChange = (event) => {
     this.setState({
-      [event.target.name]: event.target.value + "'s"
+      [event.target.name]: event.target.value + "'s "
     })
   }
-  */
+
+  
   
   render() {
     
     if (this.state.isSubmitted === false) {
       return <Intro />
-    } 
-    return <List />
+    } else if (this.state.isSubmitted === true) {
+      return <List />
+    }
+    
     
   }
 }
