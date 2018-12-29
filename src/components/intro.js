e562a52677aaf4b78d3ff39de8c218ef3a831975
Eric Butler <ericbutler000@gmail.com>
2018-12-29 04:20:23 +0000

    Took a stab at hoisting state pieces up into App and having App hand handler functions and props down to List and Intro

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,35 +1,34 @@
 import React, { Component } from 'react';
 import './intro.css'
 
-class Intro extends Component{
-    constructor(props) {
-        super(props);
-    
-        this.state = {
-          name: "",
-          isSubmitted: false
-        }
-      }
-    
+class Intro extends Component {
+    handleSubmit = (event) => {
+        this.props.onSubmit()
+    }
+
+    handleChange = (event) => {
+       this.props.onChange(event.target.value);
+    }
+
     render() {
-        return(
-        <div>
-            
+        return (
+            <div>
+
                 <div className="header">
-                    <h1>{this.state.name}To Do List</h1>
+                    <h1>{this.props.name}'s To Do List</h1>
                 </div>
-            
+
                 <div className="body">
                     <h2>What is your name?</h2>
-            
-                    <input type="text" placeholder="My First Name" 
-                         name="name" onChange={this.handleChange}>
+
+                    <input type="text" placeholder="My First Name"
+                        name="name" onInput={this.handleChange}>
                     </input>
-                    <button onClick={this.handleSubmit} 
-                    className="btn">Submit</button>
+                    <button onClick={this.handleSubmit}
+                        className="btn">Submit</button>
 
                 </div>
-        </div>
+            </div>
         )
     }
 }
